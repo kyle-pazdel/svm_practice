2ebc52030de92e577e38f42804c576ea955319c9
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-04-04 10:46:46 -0700

    Init

---
+++ svm.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import statsmodels.ap as sm
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split as tts
+from sklearn.metrics import accuracy_score
+from sklearn.utils import shuffle
+
+
+def remove_correlated_features(X):
+
+
+def remove_less_significant_features(X, Y):
+
+
+def compute_cost(W, X, Y):
+def calculate_cost_gradient(W, X_batch, Y_batch):
+def sgd(features, outputs):
+
+
+def init():
