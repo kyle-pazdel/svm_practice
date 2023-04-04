74471de0654fe4331d427eb39a4b16b4518f1d20
Kyle Pazdel <kyle.kayvaun@gmail.com>
2023-04-04 11:26:32 -0700

    imports data set from csv file. normalizes data. splits data

--- svm.py
+++ svm.py
@@ -1,21 +1,33 @@
 import numpy as np
 import pandas as pd
-import statsmodels.ap as sm
+# import statsmodels.ap as sm
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.model_selection import train_test_split as tts
 from sklearn.metrics import accuracy_score
 from sklearn.utils import shuffle
 
 
-def remove_correlated_features(X):
+# def remove_correlated_features(X):
 
 
-def remove_less_significant_features(X, Y):
+# def remove_less_significant_features(X, Y):
 
 
-def compute_cost(W, X, Y):
-def calculate_cost_gradient(W, X_batch, Y_batch):
-def sgd(features, outputs):
+# def compute_cost(W, X, Y):
+# def calculate_cost_gradient(W, X_batch, Y_batch):
+# def sgd(features, outputs):
 
 
 def init():
+    data = pd.read_csv('.data.csv')
+    diagnosis_map = {'M': 1, 'B': -1}
+    data['diagnosis'] = data['diagnosis'].map(diagnosis_map)
+    data.drop(data.columns[[-1, 0]], axis=1, inplace=True)
+
+    Y = data.loc[:, 'diagnosis']
+    X = data.iloc[:, 1:]
+
+    X_normalized = MinMaxScaler().fit_transform(X.values)
+    X = pd.DataFrame(X_normalized)
+
+    X.insert(loc=len(X.columns), column='intercept', value=1)
